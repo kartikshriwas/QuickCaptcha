4fb58d3135798c7b01e4aa3974da7d45bff64f03
Kartik <kartikshriwas14@gmail.com>
2025-02-19 19:02:16 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function QuickCaptcha() {
     const txtcaptcha = document.querySelector("#txtcaptcha");
     if (txtcaptcha) txtcaptcha.value = txtcaptcha.getAttribute("data-captcha")?.toUpperCase() || "";
 }
-requestAnimationFrame(QuickCaptcha);
-new MutationObserver(() => requestAnimationFrame(QuickCaptcha)).observe(document.body, { subtree: true, attributes: true });
+QuickCaptcha()
+new MutationObserver(() => QuickCaptcha()).observe(document.body, { subtree: true, attributes: true });
